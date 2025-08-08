f2ef865dfda3f64959baa97acb008fa1f5826804
Azamat-JS <azamatielts987@gmail.com>
2025-08-08 19:44:13 +0500

    Implementing next/navigation to redirect to another page

--- next-routing/src/app/page.tsx
+++ next-routing/src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
       <h1>Home page</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/_products">Products</Link>
+      <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+      <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
     </div>
   );
 }
