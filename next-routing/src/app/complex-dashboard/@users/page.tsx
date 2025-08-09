f02a84f2471e8eaae9601a99ce7efa2e3106e100
Azamat-JS <azamatielts987@gmail.com>
2025-08-09 12:07:57 +0500

    Implemented parallel routes in Nextjs

---
+++ next-routing/src/app/complex-dashboard/@users/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@/components/card'
+import React from 'react'
+
+const UsersAnalytics = () => {
+  return (
+    <Card>Users Analytics</Card>
+  )
+}
+
+export default UsersAnalytics
