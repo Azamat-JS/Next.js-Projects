a1936b9003191dbf668f6570334e7779905c491f
Azamat-JS <azamatielts987@gmail.com>
2025-08-13 23:59:15 +0500

    Query and filter logics implemented using useRouter, usePathname and useSearchParams in Nextjs on E-Commerce website

---
+++ E-Commerce/client/src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { ProductsType } from "@/types"
+
+const ProductCard = ({product} : {product: ProductsType}) => {
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
