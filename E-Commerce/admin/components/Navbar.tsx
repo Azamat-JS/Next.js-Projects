d16b13cea616e0ada6351d6f68c4cedf8bc65d3e
Azamat-JS <azamatielts987@gmail.com>
2025-08-16 12:21:33 +0500

    Implemented Dark and Light modes in Dashboard project with user dropdown menu

---
+++ E-Commerce/admin/components/Navbar.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
+import { LogOut, Moon, Settings, Sun, UserIcon } from 'lucide-react'
+import Link from 'next/link'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from './ui/button'
+import { useTheme } from 'next-themes'
+
+const Navbar = () => {
+    const {theme, setTheme} = useTheme()
+    return (
+        <nav className='p-4 flex items-center justify-between'>
+            {/* left */}
+            collapseButton
+            {/* right */}
+            <div className='flex items-center gap-4'>
+                <Link href='/'>Dashboard</Link>
+                {/* theme menu */}
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                            <span className="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => setTheme("light")}>
+                            Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("dark")}>
+                            Dark
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setTheme("system")}>
+                            System
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+                {/* user menu */}
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage className='w-6 h-7' src="https://thumbs.dreamstime.com/b/node-js-open-source-cross-platform-back-end-javascript-runtime-environment-executes-javascript-code-outside-web-204759459.jpg" />
+                            <AvatarFallback>JS</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent sideOffset={10}>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <UserIcon /> Profile
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Settings /> Settings
+                        </DropdownMenuItem>
+                        <DropdownMenuItem variant='destructive'>
+                            <LogOut /> Logout
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
