07d9725da1067d1c82b80c7e1efa5605ccc9d25b
Azamat-JS <azamatielts987@gmail.com>
2025-08-16 22:44:18 +0500

    Bar chart implemented

--- E-Commerce/admin/app/page.tsx
+++ E-Commerce/admin/app/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
-import Image from "next/image";
+import AppBarChart from "@/components/AppBarChart";
+
 
 export default function Home() {
   return (
-    <div className="">
-      
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
     </div>
   );
 }
